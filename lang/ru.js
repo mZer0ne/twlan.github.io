0000a1d46e5a56472e1063ce54810a39e7693f6e
Oleg Filippov <mZer0ne@users.noreply.github.com>
2017-12-08 14:34:16 +0200

    Add russian translation

---
+++ lang/ru.js
@@ -0,0 +1,35 @@
+{
+    "download": "Скачать",
+    "install": "Инструкция по установке",
+    "title": "Страница проекта TWLan",
+    "os": {
+        "win32": "Windows",
+        "mac": "OS X",
+        "linux": "Linux"
+    },
+    "github": {
+        "translate": "Переводы",
+        "oss": "Компоненты Open Source",
+        "issues": "Баг трекер"
+    },
+    "community": "Сообщество",
+    "news": "Новости",
+    "furtherSupport": "Дополнительная поддержка",
+    "forum": "Форум",
+    "credits": "Благодарности",
+    "allOthers": "Вся поддержка",
+    "allFormer": "Все участники",
+    "roles": {
+        "development": "Разработка",
+        "translation": "Перевод",
+        "information": "Сбор информации",
+        "testing": "Тесты",
+        "poweredon": "The base of the TWLan",
+        "postopus": "Поддержка и реализация этого проекта",
+        "inno": "За фантастические игры и создания этого проекта"
+    },
+    "steps": {
+        "detailed": "Нажмите здесь, чтобы получить подробные инструкции."
+    },
+    "license": "By using this software, you agree to the terms of the <a href=\"https://github.com/TWLan/Wiki/wiki/License\">TWLan License</a> and to the licenses of all the software it uses.<br>All individual licenses can be found in the \"license\" folder inside the download archives."
+}
